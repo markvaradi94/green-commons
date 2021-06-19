148f25bd8692cb8e76e20b80fe07eeede9b01896
Mark Varadi <mark.varadi03@gmail.com>
2021-06-19 16:36:02 +0300

    GAA-5 - added validations for account entity

--- src/main/kotlin/ro/asis/commons/exceptions/ResourceControllerAdvice.kt
+++ src/main/kotlin/ro/asis/commons/exceptions/ResourceControllerAdvice.kt
@@ -1,5 +1,6 @@
 package ro.asis.commons.exceptions
 
+import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -11,8 +12,15 @@ class ResourceControllerAdvice {
     @ResponseStatus(NOT_FOUND)
     fun handleResourceNotFoundException(exception: ResourceNotFoundException): ApiError =
         ApiError(404, exception.localizedMessage)
+
+    @ExceptionHandler(ValidationException::class)
+    @ResponseStatus(BAD_REQUEST)
+    fun handleValidationException(exception: ValidationException): ApiError =
+        ApiError(420, exception.localizedMessage)
 }
 
 class ResourceNotFoundException(message: String) : RuntimeException(message)
+
+class ValidationException(message: String) : RuntimeException(message)
 
 data class ApiError(val errorCode: Int, val message: String)
--- src/main/kotlin/ro/asis/commons/model/Cart.kt
+++ src/main/kotlin/ro/asis/commons/model/Cart.kt
@@ -2,5 +2,8 @@ package ro.asis.commons.model
 
 data class Cart(
     var providerId: String? = null,
-    var bags: MutableList<GreenBag> = mutableListOf()
+    var bags: MutableList<GreenBag> = mutableListOf(),
+    var totalPrice: Double = 0.0
+    //TODO totalPrice will be calculated from bags
+    //TODO pass totalPrice from Cart to Order when checking out
 )
