e8c249dd1a5d0411386f6714e802f635f349243d
Mark Varadi <mark.varadi03@gmail.com>
2021-07-21 16:38:17 +0300

    modified Account, added CORS config

--- src/main/kotlin/ro/asis/commons/filters/AccountFilters.kt
+++ src/main/kotlin/ro/asis/commons/filters/AccountFilters.kt
@@ -2,7 +2,7 @@ package ro.asis.commons.filters
 
 data class AccountFilters(
     var id: String? = null,
-    var username: String? = null,
+    var password: String? = null,
     var email: String? = null,
     var phoneNumber: String? = null,
     var type: String? = null
